39b5c72b4508632dedc99758e148596484f1ad01
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-07-17 20:53:53 +0530

    modified some files

--- backend/MERN-ai-ChatBot/src/connections/mongoDB.ts
+++ backend/MERN-ai-ChatBot/src/connections/mongoDB.ts
@@ -1,7 +1,7 @@
 import {connect} from "mongoose"
 export const connectToMongoDB = async () => {
     try {
-      //  await connect(process.env.MONGODB_URL);
+      await connect(process.env.MONGODB_URL);
     }catch (error){
         console.log(error);
         throw new Error("Unable to connect to MongoDB");
