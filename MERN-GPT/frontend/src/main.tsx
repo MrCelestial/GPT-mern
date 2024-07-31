84285a14e01f15ea809285bca4022401cd8ac327
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-07-31 17:56:47 +0530

    modified some files

--- MERN-GPT/frontend/src/main.tsx
+++ MERN-GPT/frontend/src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createTheme, ThemeProvider} from "@mui/material";
+import {BrowserRouter} from "react-router-dom";
 
+const theme = createTheme({
+    typography:{fontFamily:"Raleway,serif",
+        allVariants:{color:"white"},
+    },
+});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+         <App />
+      </ThemeProvider>
+    </BrowserRouter>,
   </React.StrictMode>,
 )
