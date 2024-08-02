d465560d734a3c3e435b20e8e5c8655f8ac4d394
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-08-02 13:26:53 +0530

    modified some files

---
+++ MERN-GPT/frontend/src/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import {createContext, ReactNode, useEffect, useState} from 'react';
+
+type User = {
+    username: string;
+    email: string;
+}
+
+type UserAuth ={
+    isLoggedIn: boolean;
+    user: User | null;
+    login:(username:string, email:string, password: string) => Promise<void>;
+    signup:(email:string, password: string) => Promise<void>;
+    logout:() => Promise<void>;//removes the cookies
+}
+const AuthContext = createContext<UserAuth | null>(null);
+const AuthContextProvider = ({children}: {children:ReactNode}) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [isLoggedIn, setLoggedIn] = useState(false);
+
+    useEffect(()=>{
+        //if cookies are valid
+
+    },[]);
+    const login = async (email:string, password: string) => {};
+    const signup = async (username:string, email:string, password: string) => {};
+    const logout = async () => {};
+
+    const value = {
+        user, isLoggedIn, login, signup, logout};
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+export default AuthContext;
